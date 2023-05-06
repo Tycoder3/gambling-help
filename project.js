1f60316b3236c2e005c58e3e2cb9e79325a5a47d
tyrese nwosu <tynwosu09rgmail.com>
2023-05-06 13:22:38 +0200

    idk pls help

---
+++ project.js
@@ -0,0 +1,40 @@
+// 1. Despot some money
+// 2. determine nuber of lines to bet on
+// 3. colect bet amount
+// 4. spin weel
+// 5. chek if won
+// 6. give out winnings
+// 7. play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, please try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseInt(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, please try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
+console.log("Deposit amount:", depositAmount);
+console.log("Number of lines:", numberOfLines);
